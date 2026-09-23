5ff8d63c808cd45cfaf3007d9a619aa3b2876a77
kisangho <kisangho5093@gmail.com>
2026-09-23 02:51:45 +0000

    ch3

--- chap3/prob1/main.c
+++ chap3/prob1/main.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
+#include <string.h>
+#include "copy.h"
 
-int main() {
-	printf("Hello World\n");
-	return 0;
+char line[MAXLINE];
+char longest[MAXLINE];
+
+int main(void)
+{
+    int len;
+    int max;
+
+    max = 0;
+
+    while (gets(line) != NULL) {
+
+        len = strlen(line);
+
+        if (len > max) {
+            max = len;
+            copy(line, longest);
+        }
+    }
+
+    if (max > 0)
+        printf("%s\n", longest);
+
+    return 0;
 }
